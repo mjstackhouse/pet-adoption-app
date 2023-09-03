e27e92c85d5b6bc3cc8b6fae54f085c81a86adfb
mjstackhouse <micahjstackhouse@gmail.com>
2023-09-02 21:12:08 -0600

    fix: Fixed the 'signin-button-popup' appearing over every animal in the search results. It's been replaced by the regular 'signin-form' component so that the user can sign-in right there on the page, instead of navigating away to the actual 'signin' route.

--- src/components/signin-form.js
+++ src/components/signin-form.js
@@ -1,29 +1,67 @@
 'use client'
 
 import Link from 'next/link';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { signIn } from 'next-auth/react';
 import { useSearchParams } from 'next/navigation';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebook, faGoogle } from '@fortawesome/free-brands-svg-icons';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope, faXmark } from '@fortawesome/free-solid-svg-icons';
+import $ from 'jquery';
+import { Bree_Serif } from 'next/font/google';
 
-export default function SigninForm() {
+const bree = Bree_Serif({ weight: '400', subsets: ['latin'] });
+
+export default function SigninForm({ parameters }) {
+
   const [userEmail, setUserEmail] = useState(null);
 
   const searchParams = useSearchParams();
   const callbackUrl = searchParams.get('callbackUrl');
 
-  console.log('callbackUrl: ', callbackUrl);
+  useEffect(() => {
+    
+    if (callbackUrl !== null) {
+      $('#close-signin-btn').css('display', 'none');
+      $('.signin-form-container').eq(0).addClass('fixed h-[100vh] w-screen md:h-auto md:w-auto md:inset-x-[5%] lg:inset-x-[15%] md:inset-y-[25%] z-30 hidden rounded-none md:rounded-3xl');
+      
+      $('.signin-form-container').eq(1).removeClass('hidden');
+      $('.signin-form-container').eq(1).addClass('mx-4 sm:mx-8 rounded-3xl');
+    }
+    else {
+      $('.signin-form-container').eq(1).removeClass('mx-4 sm:mx-8 rounded-3xl');
+      $('.signin-form-container').addClass('fixed h-[100vh] w-screen md:h-auto md:w-auto md:inset-x-[5%] lg:inset-x-[15%] md:inset-y-[25%] z-30 hidden rounded-none md:rounded-3xl');
+    }
+
+    if (document.getElementById('inner-body-container') !== null) {
+      document.getElementById('inner-body-container').addEventListener('click', () => {
+        closeFilters();
+      })
+    }
+  }, []);
+
+  function closeFilters() {
+    $('.signin-form-container').eq(0).css('display', 'none');
+    $('#inner-body-container').css('filter', 'brightness(1)');
+    $('#inner-body-container *').css('pointer-events', 'auto');
+    $('#inner-body-container *').css('touch-action', 'auto');
+    $('html').css('overflow', 'visible');
+  }
 
   return (
-    <div className='max-h-[60%] sm:max-w-[50vw] flex flex-wrap justify-start items-center bg-white text-base max-w-fit mx-4 sm:mx-8 px-4 py-6 sm:px-6 sm:py-8 rounded-3xl shadow-lg'>
+    <div className='signin-form-container md:max-w-[700px] lg:max-w-[850px] md:mx-auto flex flex-wrap place-content-center bg-white text-base max-w-fit px-4 py-6 sm:px-6 sm:py-8 shadow-lg hidden'>
+      <button id='close-signin-btn' className='close-signin-btn absolute top-[1rem] left-[1rem] mb-4' onClick={() => closeFilters()}>
+        <FontAwesomeIcon icon={faXmark} className='h-[2rem] text-black hover:text-darker-gray' />
+      </button>
+      <h1 id='signin-to-favorite' className={`${bree.className} signin-to-favorite text-center mx-auto font-bold text-3xl sm:text-4xl leading-loose tracking-wide mb-8 hidden leading-snug`}>
+          Sign in to favorite animals
+      </h1>
       <div className='basis-full text-center mb-6'>
-        <button className='font-bold tracking-wider bg-blue hover:bg-darker-blue hover:shadow-md text-black px-4 py-2 rounded-3xl mb-4 sm:mr-2' onClick={() => signIn('google', { callbackUrl })}>
+        <button id='google-signin-btn' className='sm:mr-2 block sm:inline font-bold tracking-wider bg-blue hover:bg-darker-blue hover:shadow-md hover:underline underline-offset-4 text-black px-4 py-2 rounded-3xl mb-4 mx-auto' onClick={() => { callbackUrl !== null ? signIn('google', { callbackUrl }) : signIn('google') } }>
           <FontAwesomeIcon icon={faGoogle} className='h-[1rem] mr-2' />
           Continue with Google
         </button>
-        <button className='font-bold tracking-wider bg-blue hover:bg-darker-blue hover:shadow-md text-black px-4 py-2 rounded-3xl sm:ml-2' onClick={() => signIn('facebook', { callbackUrl })}>
+        <button id='facebook-signin-btn' className='sm:ml-2 block sm:inline font-bold tracking-wider bg-blue hover:bg-darker-blue hover:shadow-md hover:underline underline-offset-4 text-black px-4 py-2 rounded-3xl mx-auto' onClick={() => { callbackUrl !== null ? signIn('facebook', { callbackUrl }) : signIn('facebook') }}>
           <FontAwesomeIcon icon={faFacebook} className='h-[1rem] mr-2' />
           Continue with Facebook
         </button>
